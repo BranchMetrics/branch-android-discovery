a92ede8617e4bf8fbc83270101f82dbd4c632234
bklastaitis-branch <benas.klastaitis@branch.io>
2020-04-29 16:04:55 -0700

    gzip payload

--- Analytics/src/main/java/io/branch/sdk/android/search/analytics/AnalyticsUtil.java
+++ Analytics/src/main/java/io/branch/sdk/android/search/analytics/AnalyticsUtil.java
@@ -2,9 +2,8 @@ package io.branch.sdk.android.search.analytics;
 
 import android.util.Log;
 
-import java.io.BufferedOutputStream;
-import java.io.OutputStream;
 import java.net.URL;
+import java.util.zip.GZIPOutputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -16,29 +15,30 @@ public class AnalyticsUtil {
 
     static void makeUpload(String veryLongString) {
 
-        int maxLogSize = 1000;
-        for(int i = 0; i <= veryLongString.length() / maxLogSize; i++) {
-            int start = i * maxLogSize;
-            int end = (i+1) * maxLogSize;
-            end = end > veryLongString.length() ? veryLongString.length() : end;
-            Log.i(LOGTAG, veryLongString.substring(start, end));
-        }
-//        HttpsURLConnection urlConnection = null;
-//        try {
-//            urlConnection = (HttpsURLConnection) new URL(analyticsUploadUrl).openConnection();
-//            urlConnection.setDoOutput(true);
-//            urlConnection.setChunkedStreamingMode(0);
-//
-//            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-//            out.write(payload.getBytes());
-//            out.flush();
-//        } catch (Exception e) {
-//            Log.i(LOGTAG, "exception uploading = " + e.getMessage());
-//            e.printStackTrace();
-//        } finally {
-//            if (urlConnection != null) {
-//                urlConnection.disconnect();
-//            }
+//        int maxLogSize = 1000;
+//        for(int i = 0; i <= veryLongString.length() / maxLogSize; i++) {
+//            int start = i * maxLogSize;
+//            int end = (i+1) * maxLogSize;
+//            end = end > veryLongString.length() ? veryLongString.length() : end;
+//            Log.i(LOGTAG, veryLongString.substring(start, end));
 //        }
+
+        byte[] data = veryLongString.getBytes();
+        HttpsURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpsURLConnection) new URL(analyticsUploadUrl).openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setFixedLengthStreamingMode(data.length);
+            GZIPOutputStream gzipOutputStream = new GZIPOutputStream(urlConnection.getOutputStream());
+            gzipOutputStream.write(data);
+            gzipOutputStream.flush();
+        } catch (Exception e) {
+            Log.i(LOGTAG, "exception uploading = " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
     }
 }
