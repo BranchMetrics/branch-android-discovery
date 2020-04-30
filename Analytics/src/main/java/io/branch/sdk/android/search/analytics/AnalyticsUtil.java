5cfc3a780f60ad77b30d5afccf32367ad1d46b80
bklastaitis-branch <benas.klastaitis@branch.io>
2020-04-30 15:59:39 -0700

    fixed upload method

--- Analytics/src/main/java/io/branch/sdk/android/search/analytics/AnalyticsUtil.java
+++ Analytics/src/main/java/io/branch/sdk/android/search/analytics/AnalyticsUtil.java
@@ -1,5 +1,7 @@
 package io.branch.sdk.android.search.analytics;
 
+import android.os.AsyncTask;
+
 import java.net.URL;
 import java.util.zip.GZIPOutputStream;
 
@@ -8,9 +10,9 @@ import javax.net.ssl.HttpsURLConnection;
 import static io.branch.sdk.android.search.analytics.BranchAnalytics.Logd;
 
 public class AnalyticsUtil {
-    private static final String analyticsUploadUrl = "https://fakeUrl.fakeUrl";
+    private static final String analyticsUploadUrl = "https://9b3a54d7.ngrok.io/upload";
 
-    static void makeUpload(String veryLongString) {
+    static void makeUpload(final String veryLongString) {
 
         if (BranchAnalytics.loggingEnabled) {
             // todo fix this so it doesn't print across multiple lines
@@ -23,27 +25,37 @@ public class AnalyticsUtil {
             }
         }
 
-        byte[] data = veryLongString.getBytes();
-        HttpsURLConnection urlConnection = null;
-        try {
-            urlConnection = (HttpsURLConnection) new URL(analyticsUploadUrl).openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.setFixedLengthStreamingMode(data.length);
-            GZIPOutputStream gzipOutputStream = new GZIPOutputStream(urlConnection.getOutputStream());
-            gzipOutputStream.write(data);
-            gzipOutputStream.flush();
-            if (urlConnection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-                Logd("analytics upload was successful");
-            } else {
-                // todo add retry logic
+        new AsyncTask<String, Void, Void>() {
+            @Override
+            protected Void doInBackground(String... strings) {
+                byte[] data = veryLongString.getBytes();
+                HttpsURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpsURLConnection) new URL(analyticsUploadUrl).openConnection();
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setChunkedStreamingMode(0);// 0 = default size chunks
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.setRequestProperty("Content-Encoding", "gzip");
+                    GZIPOutputStream gzipOutputStream = new GZIPOutputStream(urlConnection.getOutputStream());
+                    gzipOutputStream.write(data);
+                    gzipOutputStream.close();
+                    if (urlConnection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                        Logd("analytics upload was successful");
+                    } else {
+                        Logd("analytics upload was not successful, status: " + urlConnection.getResponseCode());
+                        // todo add retry logic?
+                    }
+                } catch (Exception e) {
+                    Logd("exception when uploading: " + e.getMessage());
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+                return null;
             }
-        } catch (Exception e) {
-            Logd("exception when uploading: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
+        }.execute(veryLongString);
     }
 }
